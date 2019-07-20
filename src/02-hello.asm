f8bc0e16037b3b22894ca13c52f25932c3de0e03
707 <707472783@qq.com>
2019-07-21 01:18:54 +0800

    添加三小结内容

---
+++ src/02-hello.asm
@@ -0,0 +1,17 @@
+mov ah, 0x0e ; tty mode
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10 ; 'l' is still on al, remember?
+mov al, 'o'
+int 0x10
+; 上面的操作都是在cpu上执行的
+
+jmp $ ; jump to current address = infinite loop
+
+; padding and magic number
+times 510 - ($-$$) db 0
+dw 0xaa55
