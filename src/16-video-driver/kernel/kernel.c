f2790fb9cbc7e1e0321f0f03451c1170192874e1
707 <707472783@qq.com>
2019-08-02 00:07:30 +0800

    add 21

--- src/16-video-driver/kernel/kernel.c
+++ src/16-video-driver/kernel/kernel.c
@@ -3,8 +3,8 @@
 void main() {
     clear_screen();
     kprint_at("FFFOS kernel is running", -1, -1);
-    kprint_at("This text spans multiple lines", 75, 10);    // 线性的可以跨行
-    kprint_at("There is a line\nbreak", 0, 20);
-    kprint("There is a line\nbreak");
-    kprint_at("What happens when we run out of space?", 45, 24);
+    // kprint_at("This text spans multiple lines", 75, 10);    // 线性的可以跨行
+    // kprint_at("There is a line\nbreak", 0, 20);
+    // kprint("There is a line\nbreak");
+    // kprint_at("What happens when we run out of space?", 45, 24);
 }
