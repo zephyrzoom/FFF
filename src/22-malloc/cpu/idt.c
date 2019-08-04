f2e1010bde5152ea9c074c8e92b63e0d6bbcdd0a
707 <707472783@qq.com>
2019-08-04 13:47:02 +0800

    修复23中的bug

--- src/22-malloc/cpu/idt.c
+++ src/22-malloc/cpu/idt.c
@@ -1,6 +1,6 @@
 #include "idt.h"
 
-void set_idt_gate(int n, u32 handler) {
+void set_idt_gate(int n, uint32_t handler) {
     idt[n].low_offset = low_16(handler);    // 中断函数地址的高位
     idt[n].sel = KERNEL_CS; // 中断函数选择器，GDT或LDT中的代码段选择器
     idt[n].always0 = 0; // 8个
@@ -13,12 +13,12 @@ void set_idt_gate(int n, u32 handler) {
 }
 
 void set_idt() {
-    idt_reg.base = (u32) &idt;  // 不带&也正确执行，idt本来就是地址啊，为什么多加个&，不明白
+    idt_reg.base = (uint32_t) &idt;  // 不带&也正确执行，idt本来就是地址啊，为什么多加个&，不明白
     idt_reg.limit = IDT_ENTRIES * sizeof(idt_gate_t) - 1;
     /* Don't make the mistake of loading &idt -- always load &idt_reg */
     /* __volatile__不让编译器优化该条指令
      * r是通用寄存器
      * %0-%9表示对应的操作数，在这里%0就是&idt_reg
      */
-    __asm__ __volatile__("lidtl (%0)" : : "r" (&idt_reg));
+    asm volatile("lidtl (%0)" : : "r" (&idt_reg));
 }
--- src/22-malloc/cpu/ports.c
+++ src/22-malloc/cpu/ports.c
@@ -15,7 +15,7 @@ unsigned char port_byte_in (unsigned short port) {
      * 内联汇编格式：指令:输出:输入
      * a代表eax，d代表edx
      */
-    __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
+    asm("in %%dx, %%al" : "=a" (result) : "d" (port));
     return result;
 }
 
@@ -26,15 +26,15 @@ void port_byte_out (unsigned short port, unsigned char data) {
      * and none in the 'return' area
      * 将data写入port，就是把al的值写入dx端口
      */
-    __asm__("out %%al, %%dx" : : "a" (data), "d" (port));
+    asm("out %%al, %%dx" : : "a" (data), "d" (port));
 }
 
 unsigned short port_word_in (unsigned short port) {
     unsigned short result;
-    __asm__("in %%dx, %%ax" : "=a" (result) : "d" (port));
+    asm("in %%dx, %%ax" : "=a" (result) : "d" (port));
     return result;
 }
 
 void port_word_out (unsigned short port, unsigned short data) {
-    __asm__("out %%ax, %%dx" : : "a" (data), "d" (port));
+    asm("out %%ax, %%dx" : : "a" (data), "d" (port));
 }
