dbb117491aa7b1dc54d87c91bdcffec06de8be61
707 <707472783@qq.com>
2019-08-24 17:17:07 +0800

    彩虹字符

--- Makefile
+++ Makefile
@@ -4,6 +4,17 @@ CFLAGS = -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 all:
 	${AS} boot.s -o boot.o
 	${GCC} ${CFLAGS} -c kernel.c -o kernel.o
+	${GCC} -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+	cp myos.bin isodir/boot/myos.bin
+	cp grub.cfg isodir/boot/grub/grub.cfg
+	grub-mkrescue -o myos.iso isodir
+	qemu-system-i386 -cdrom myos.iso
+valid:
+	if [ -z $(grub-file --is-x86-multiboot myos.bin) ];then	\
+		echo "multiboot confirmed";							\
+	else													\
+		echo "the file is not multiboot";					\
+	fi
 
 clean:
 	rm -rf *.o
--- kernel.c
+++ kernel.c
@@ -1,7 +1,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
- 
+
 /* Check if the compiler thinks you are targeting the wrong operating system. */
 #if defined(__linux__)
 #error "You are not using a cross-compiler, you will most certainly run into trouble"
@@ -11,9 +11,10 @@
 #if !defined(__i386__)
 #error "This tutorial needs to be compiled with a ix86-elf compiler"
 #endif
- 
+
 /* Hardware text mode color constants. */
-enum vga_color {
+enum vga_color
+{
 	VGA_COLOR_BLACK = 0,
 	VGA_COLOR_BLUE = 1,
 	VGA_COLOR_GREEN = 2,
@@ -31,84 +32,112 @@ enum vga_color {
 	VGA_COLOR_LIGHT_BROWN = 14,
 	VGA_COLOR_WHITE = 15,
 };
- 
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) 
+
+static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg)
 {
 	return fg | bg << 4;
 }
- 
-static inline uint16_t vga_entry(unsigned char uc, uint8_t color) 
+
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
 {
-	return (uint16_t) uc | (uint16_t) color << 8;
+	return (uint16_t)uc | (uint16_t)color << 8;
 }
- 
-size_t strlen(const char* str) 
+
+size_t strlen(const char *str)
 {
 	size_t len = 0;
 	while (str[len])
 		len++;
 	return len;
 }
- 
+
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
- 
+
 size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
-uint16_t* terminal_buffer;
- 
-void terminal_initialize(void) 
+uint16_t *terminal_buffer;
+
+void terminal_initialize(void)
 {
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-	terminal_buffer = (uint16_t*) 0xB8000;
-	for (size_t y = 0; y < VGA_HEIGHT; y++) {
-		for (size_t x = 0; x < VGA_WIDTH; x++) {
+	terminal_buffer = (uint16_t *)0xB8000;
+	for (size_t y = 0; y < VGA_HEIGHT; y++)
+	{
+		for (size_t x = 0; x < VGA_WIDTH; x++)
+		{
 			const size_t index = y * VGA_WIDTH + x;
 			terminal_buffer[index] = vga_entry(' ', terminal_color);
 		}
 	}
 }
- 
-void terminal_setcolor(uint8_t color) 
+
+void terminal_setcolor(uint8_t color)
 {
 	terminal_color = color;
 }
- 
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
+
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 {
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = vga_entry(c, color);
 }
- 
-void terminal_putchar(char c) 
+
+void scroll_up(void)
 {
-	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
-		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
+	for (size_t i = 0; i < (VGA_HEIGHT - 1) * VGA_WIDTH; i++)
+	{
+		terminal_buffer[i] = terminal_buffer[i + VGA_WIDTH];
 	}
 }
- 
-void terminal_write(const char* data, size_t size) 
+
+void terminal_putchar(char c)
+{
+	if (c == '\n')
+	{
+		terminal_column = 0;
+		if (++terminal_row == VGA_HEIGHT)
+		{
+			scroll_up();
+			terminal_row = VGA_HEIGHT - 1;
+		}
+	}
+	else
+	{
+		uint8_t random_color = vga_entry_color(terminal_row % (VGA_COLOR_WHITE + 1), terminal_column % (VGA_COLOR_WHITE + 1));
+		terminal_setcolor(random_color);
+		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+		if (++terminal_column == VGA_WIDTH)
+		{
+			terminal_column = 0;
+			if (++terminal_row == VGA_HEIGHT)
+			{
+				scroll_up();
+				terminal_row = VGA_HEIGHT - 1;
+			}
+		}
+	}
+}
+
+void terminal_write(const char *data, size_t size)
 {
 	for (size_t i = 0; i < size; i++)
 		terminal_putchar(data[i]);
 }
- 
-void terminal_writestring(const char* data) 
+
+void terminal_writestring(const char *data)
 {
 	terminal_write(data, strlen(data));
 }
- 
-void kernel_main(void) 
+
+void kernel_main(void)
 {
 	/* Initialize terminal interface */
 	terminal_initialize();
- 
+
 	/* Newline support is left as an exercise. */
-	terminal_writestring("Hello, kernel World!\n");
+	terminal_writestring("1.Hello, kernel World!\n2.This is FFFOS\n3.Hello, kernel World!\n4.This is FFFOS\n5.Hello, kernel World!\n6.This is FFFOS\n7.Hello, kernel World!\n8.This is FFFOS\n9.Hello, kernel World!\n10.This is FFFOS\n11.Hello, kernel World!\n12.This is FFFOS\n13.Hello, kernel World!\n14.This is FFFOS\n15.Hello, kernel World!\n16.This is FFFOS\n17.Hello, kernel World!\n18.This is FFFOS\n19.Hello, kernel World!\n20.This is FFFOS\n21.Hello, kernel World!\n22.This is FFFOS\n23.Hello, kernel World!\n24.This is FFFOS\n25.Hello, kernel World!\n26.This is FFFOS\n27.Hello, kernel World!\n28.This is FFFOS\n");
 }
